75dc21c288ff592dc91411b958491ccec878f919
srk-333 <sauravmk333@gmail.com>
2022-02-16 09:20:04 +0530

    [Saurav]Add:welcome message

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome into Cab invoice generator program");
+            Console.ReadKey(); 
+        }
+    }
+}
